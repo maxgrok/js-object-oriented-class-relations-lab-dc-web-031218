7623be0accd8f5b0094c9b8db350f6a339988dfd
github username <github email address>
2018-05-16 17:57:08 -0400

    Done.

--- index.js
+++ index.js
@@ -6,25 +6,24 @@ store.trips = []
 let driverId = 0; 
 
 class Driver {
-	constructor(name, driverId, trips){
+	constructor(name){
 		this.name = name
 		this.id = ++driverId
-		this.trips = trips
-		store.trips.push(this.trips)
+
 		store.drivers.push(this)
+	}
+	
+	passengers(){
+		return this.trips().map(trip =>{
+				return trip.passenger()
+		})//all of the passengers that a driver has taken on a trip
 	}
 
 	trips(){
 		//iterate through trips
 		return store.trips.filter(trip=>{
-			trip.driverId === this.id 
+			return trip.driverId === this.id
 		})
-	}
-
-	passengers(){
-		return store.passengers.filter(passenger =>{
-			passenger.driverId === this.id
-		})//all of the passengers that a driver has taken on a trip
 	}
 
 }
@@ -32,19 +31,19 @@ class Driver {
 let passengerId = 0;
 
 class Passenger {
-	constructor (name, passengerId, driverId){
+	constructor (name){
 		this.name = name
 		this.id = ++passengerId
-		this.driverId = driverId
 		store.passengers.push(this)
 	}
 
 	trips(){
-		return // all of the trips the passenger has taken
+		return store.trips.filter(trip => this.id === trip.passengerId) // all of the trips the passenger has taken
 	}
 
 	drivers(){
-		return // all of the drivers that the
+		return this.trips().map(trip => trip.driver())
+		//all of the drivers that the
 		// passenger has been taken on a trip
 	}
 }
@@ -52,18 +51,18 @@ class Passenger {
 let tripId = 0;
 
 class Trip {
-	constructor(driverId, passengerId, tripId){
+	constructor(driver, passenger){
 		this.id = ++tripId
-		this.driverId = driverId
-		this.passengerId = passengerId 
+		this.driverId = driver.id 
+		this.passengerId = passenger.id
 		store.trips.push(this)
 	}
 
 	driver(){
-		return // driver associated with the trip
+		return store.drivers.find(driver => driver.id === this.driverId) // driver associated with the trip
 	}
 
 	passenger(){
-		return // the passenger associated with the trip
+		return store.passengers.find(passenger => passenger.id === this.passengerId) // the passenger associated with the trip
 	}
 }
